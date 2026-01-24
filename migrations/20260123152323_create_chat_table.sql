4d1ed398039bf0399a5cd6309d9691b7adfe70c7
Vova <YesItsVova@mail.ru>
2026-01-25 02:05:31 +0300

    Time value added in migration file

--- migrations/20260123152323_create_chat_table.sql
+++ migrations/20260123152323_create_chat_table.sql
@@ -12,9 +12,9 @@ CREATE INDEX IF NOT EXISTS idx_chat_title ON chat(title);
 
 INSERT INTO chat (title, created_at)
 VALUES
-    ('first chat', '01-01-2021'),
-    ('second chat', '05-08-2022'),
-    ('third chat','10-21-2025');
+    ('first chat', '01-02-2021 04:12:27'),
+    ('second chat', '10-31-2022 21:49:39'),
+    ('third chat','10-21-2025 16:06:14');
 
 -- +goose Down
 -- +goose StatementBegin
